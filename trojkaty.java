e6d56a516d482f7d96c4ae8601d468cbd2775f5f
Jan Czubiński <jczubins@artemis.wszib.edu.pl>
2022-05-30 09:46:41 +0200

    Optymalizacja: uwspólnienie kodu

--- trojkaty.java
+++ trojkaty.java
@@ -33,6 +33,37 @@ class Trojkaty {
         System.out.println("Program do rozpoznawania rodzaju trójkąra");
         System.out.println("Uruchom z trzema argumentami liczbowymi - długość boków trójkąta");
     }
+
+/**
+                * Funkcja zwraca ile par odcinków trójkąta ma jednakową długość.
+                * @param {float} a - Długość pierwszego boku.
+                * @param {float} b - Długość drugiego boku.
+                * @param {float} c - Długość trzeciego boku.
+                */
+               public static int ileJednakowych(float a, float b, float c){
+                   int jednakowe = 0; // tutaj zliczamy ile jest jednakowych bokow
+                   if (a == b) {
+                      jednakowe++;
+                   }
+                   if (b == c) {
+                      jednakowe++;
+                   }
+                   return jednakowe;
+               }
+               /**
+                * Główna funkcja programu.
+                * @param {float} a - Długość pierwszego boku.
+                * @param {float} b - Długość drugiego boku.
+                * @param {float} c - Długość trzeciego boku.
+                */
+               public static void jakiTrojkat(float a, float b, float c){
+                   int jednakoweDlugosci = ileJednakowych(a, b, c); // tutaj zliczamy ile jest jednakowych bokow
+                   if (jednakoweDlugosci > 1) {
+                       System.out.println("Trójkąt równoboczny"); 
+                   } else if (jednakoweDlugosci > 0) {
+                       System.out.println("Trójkąt równoramienny");
+                   }
+                   ...
     /** Glowna funkcja */
     public static void main(String... args) {
         if (args.length != 3) {
