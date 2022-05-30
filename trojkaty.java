8e6108af4b6454aad4413b86554abf4ed8ee1ab3
Jan Czubiński <jczubins@artemis.wszib.edu.pl>
2022-05-30 09:44:19 +0200

    Weryfikacja wejścia: warunek istnienia trójkąta.

--- trojkaty.java
+++ trojkaty.java
@@ -33,6 +33,30 @@ class Trojkaty {
         System.out.println("Program do rozpoznawania rodzaju trójkąra");
         System.out.println("Uruchom z trzema argumentami liczbowymi - długość boków trójkąta");
     }
+
+/**
+                * Funkcja do sprawdzenia czy trójkąt o podanych bokach może zostać zbudowany.
+                * @param {float} a - Długość pierwszego boku.
+                * @param {float} b - Długość drugiego boku.
+                * @param {float} c - Długość trzeciego boku.
+                */
+               public static boolean czyIstniejeTrojkat(float a, float b, float c){
+                  if (a + b <= c) return false;
+                  if (a + c <= b) return false;
+                  if (b + c <= a) return false;
+                  return true;
+               }
+               /** Glowna funkcja */
+               public static void main(String... args) {
+                   ...
+                   if (!czyIstniejeTrojkat(a, b, c)) {
+                    System.out.println("Z podanych długości boków nie da się zbudować trójkąta");
+                    System.exit(3);
+                   }
+                   ...
+               }
+            
+
     /** Glowna funkcja */
     public static void main(String... args) {
         if (args.length != 3) {
